07a09db067a3f6a650841d3ba2d276957f6df001
Alberto <tejotaesi@gmail.com>
2023-10-17 17:28:11 -0300

    feat: built landing page

---
+++ assets/scripts/index.js
@@ -0,0 +1,7 @@
+function showGamePage() {
+  const landingpage = document.querySelector('.landing-page');
+  const gamePage = document.querySelector('.game-page');
+
+  landingpage.classList.add('hidden');
+  gamePage.classList.remove('hidden');
+}
