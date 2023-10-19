cf29a2afb91edc838b82a1822c7fa654da10d039
Alberto <tejotaesi@gmail.com>
2023-10-18 21:55:23 -0300

    feat: Built modal layout

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,7 +1,14 @@
 function showGamePage() {
   const landingpage = document.querySelector('.landing-page');
   const gamePage = document.querySelector('.game-page');
+  const overlay = document.querySelector('.overlay');
 
   landingpage.classList.add('hidden');
   gamePage.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+function closeModal() {
+  const overlay = document.querySelector('.overlay'); 
+  overlay.classList.add('hidden');
 }
